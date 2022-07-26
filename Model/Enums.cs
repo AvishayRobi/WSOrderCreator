1a72024149002e198d33246e4cc02e00d03141e4
AvishayRobi <79645481+AvishayRobi@users.noreply.github.com>
2022-07-26 12:01:15 +0300

    Create Enums.cs

---
+++ Model/Enums.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSOrderCreator.Model
+{
+  public enum eItemType
+  {
+    /// <summary>
+    /// Item Type is Product
+    /// </summary>
+    Product = 0,
+
+    /// <summary>
+    /// Item Type is Coupon
+    /// </summary>
+    Coupon = 1
+  }
+}
