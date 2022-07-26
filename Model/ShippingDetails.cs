e5a3b366587e78b7a4e3ef881e3751e805320bbb
AvishayRobi <79645481+AvishayRobi@users.noreply.github.com>
2022-07-26 12:01:53 +0300

    Create ShippingDetails.cs

---
+++ Model/ShippingDetails.cs
@@ -0,0 +1,19 @@
+namespace WSOrderCreator.Model
+{
+  public class ShippingDetails
+  {
+    public string City { get; set; }
+
+    public string StreetName { get; set; }
+
+    public string StreetNumber { get; set; }
+
+    public int ZipCode { get; set; }
+
+    public string Enterance { get; set; }
+
+    public int Floor { get; set; }
+
+    public int Apratment { get; set; }
+  }
+}
